abed0f5a2eb43162538e0da35f4e77595247bb65
Culpur.net <30544791+culpur@users.noreply.github.com>
2026-04-14 12:39:07 +0200

    formula: update SHA256 for v2.1.4 post-release fixes

--- Formula/anvil.rb
+++ Formula/anvil.rb
@@ -3,32 +3,28 @@ class Anvil < Formula
   homepage "https://culpur.net/anvil"
   version "2.1.4"
   license "Proprietary"
-
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/culpur/anvil/releases/download/v2.1.4/anvil-aarch64-apple-darwin"
-      sha256 "ce70b0855eff9dcc7311f425fcf4f5b4437cf03bc2d688d449aa8c2579d66864"
+      sha256 "63845f7856c2747252d5230bbf8e5e26544a7881806926e1efe3adbb25d0182e"
     else
       url "https://github.com/culpur/anvil/releases/download/v2.1.4/anvil-x86_64-apple-darwin"
-      sha256 "b0edc1d72d641833758894bf907bdc6ee3e5845f628ab6b19e1e7cc56cf7a7aa"
+      sha256 "93de87e30c649e7700646f2b79a0b107088f521235d1f907360e0dbe7ff11730"
     end
   end
-
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/culpur/anvil/releases/download/v2.1.4/anvil-aarch64-unknown-linux-gnu"
-      sha256 "3605e2acd457dc77e99457afcb13ee1211fea1bc6859980a1374f597fbe412b6"
+      sha256 "cdc846769d8ff7a61355a18a885ad5fb3c90f2181d09c9d26e1f548e307868b0"
     else
       url "https://github.com/culpur/anvil/releases/download/v2.1.4/anvil-x86_64-unknown-linux-gnu"
-      sha256 "5f6b1ce081a6436e9a6a646d9577c1be2de38849a18df2b40bb5042c68ea2103"
+      sha256 "704491a2ca435e262108c3129eecbe04472f2478a14c6fdc03a908d4820c4af1"
     end
   end
-
   def install
     downloaded = Dir["anvil-*"].first || "anvil"
     bin.install downloaded => "anvil"
   end
-
   test do
     assert_match "Anvil CLI", shell_output("#{bin}/anvil --version")
   end
