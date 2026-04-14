3d143098c9a8c129df79797c553e08ab9fc3cadb
Culpur.net <30544791+culpur@users.noreply.github.com>
2026-04-14 14:35:47 +0200

    formula: bump to v2.2.1

--- Formula/anvil.rb
+++ Formula/anvil.rb
@@ -1,24 +1,24 @@
 class Anvil < Formula
   desc "AI coding assistant with typed credential vault, live remote control, 5 providers"
   homepage "https://culpur.net/anvil"
-  version "2.2.0"
+  version "2.2.1"
   license "Proprietary"
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/culpur/anvil/releases/download/v2.2.0/anvil-aarch64-apple-darwin"
-      sha256 "4d0fd9cf2364e3f03695c2bcf86c238049137f078dbdc0bd52a99838832f9c6e"
+      url "https://github.com/culpur/anvil/releases/download/v2.2.1/anvil-aarch64-apple-darwin"
+      sha256 "3432821709bcca5782efe5d2e4b827d126c9a434f80cee433e4d97268b769f8f"
     else
-      url "https://github.com/culpur/anvil/releases/download/v2.2.0/anvil-x86_64-apple-darwin"
-      sha256 "ac2761dbceba809009a34856b06d0383ca963e8c52cd0c5050921fbe9e565a04"
+      url "https://github.com/culpur/anvil/releases/download/v2.2.1/anvil-x86_64-apple-darwin"
+      sha256 "f619289a3c7b9a7b092888bcff494fcc02434f681ad56db85108eba230703471"
     end
   end
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/culpur/anvil/releases/download/v2.2.0/anvil-aarch64-unknown-linux-gnu"
-      sha256 "9317441e1b34aeb3f1014372f8b29b9b46955fc3b238dcf7a15b9a2425ac8e5c"
+      url "https://github.com/culpur/anvil/releases/download/v2.2.1/anvil-aarch64-unknown-linux-gnu"
+      sha256 "285d578314ad467f4da335e45eea5cc1fd4123281ae88af2fbef7eb999074a8e"
     else
-      url "https://github.com/culpur/anvil/releases/download/v2.2.0/anvil-x86_64-unknown-linux-gnu"
-      sha256 "64470a74ada43cbd5c6a5495b5ce4c17e4895dec177dd3a78a838dac5f8b809f"
+      url "https://github.com/culpur/anvil/releases/download/v2.2.1/anvil-x86_64-unknown-linux-gnu"
+      sha256 "28c961e1106047b63e09af95b98bff13a09d7e93da2850308aa2182a4bac62e1"
     end
   end
   def install
