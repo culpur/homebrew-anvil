f48e1c6047e87acc6a295dad647772d75a797fec
Culpur.net <30544791+culpur@users.noreply.github.com>
2026-07-08 09:48:02 +0200

    formula: bump to v2.2.25

--- Formula/anvil.rb
+++ Formula/anvil.rb
@@ -1,29 +1,29 @@
 class Anvil < Formula
   desc "AI coding assistant with typed credential vault, live remote control, 5 providers"
   homepage "https://culpur.net/anvil"
-  version "2.2.24"
+  version "2.2.25"
   license "Proprietary"
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/culpur/anvil/releases/download/v2.2.24/anvil-aarch64-apple-darwin"
-      sha256 "0445c2451265fe138cbd3d916b8acdf70390cd1750ccacf235509c8b37006a6d"
+      url "https://github.com/culpur/anvil/releases/download/v2.2.25/anvil-aarch64-apple-darwin"
+      sha256 "7ba82635d34a1ca6aee0f8765891fa6e0d082a5d5ba0a4222b6426a0105f317a"
     else
-      url "https://github.com/culpur/anvil/releases/download/v2.2.24/anvil-x86_64-apple-darwin"
-      sha256 "cca2bb781be686b0c86366e84b84a689b58c373b3c15def57306f41cd6b9a3a7"
+      url "https://github.com/culpur/anvil/releases/download/v2.2.25/anvil-x86_64-apple-darwin"
+      sha256 "b59fbd70a282f06294e71cd8db195ed4679a8211e469de71302e79c62f2eadd9"
     end
   end
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/culpur/anvil/releases/download/v2.2.24/anvil-aarch64-unknown-linux-gnu"
-      sha256 "8da0f078d4c85b05a1217c18f633e024b55085cac77ba47da8a515dbe9f21b7f"
+      url "https://github.com/culpur/anvil/releases/download/v2.2.25/anvil-aarch64-unknown-linux-gnu"
+      sha256 "94d8679ca1401bc8730ba0031c714f6b74876ca4cbe9efeb4160a393ca71de62"
     else
-      url "https://github.com/culpur/anvil/releases/download/v2.2.24/anvil-x86_64-unknown-linux-gnu"
-      sha256 "f211dc5abf7806cb83b40f9a6288157ebd242ae491f21264d0593c1d4d1762c1"
+      url "https://github.com/culpur/anvil/releases/download/v2.2.25/anvil-x86_64-unknown-linux-gnu"
+      sha256 "cadb9f8fa67cb08e8d5f751016fa936285b38c0c240afcef9be6610a1257ca61"
     end
   end
   resource "manpage" do
-    url "https://github.com/culpur/anvil/releases/download/v2.2.24/anvil.1"
-    sha256 "e13656d53942dba28fbe97d6413735f9c32204c18570db4ef19f79679eb62e12"
+    url "https://github.com/culpur/anvil/releases/download/v2.2.25/anvil.1"
+    sha256 "bfd68dffeb1e021f52ae6ee47f215476bb7cb07a7733e87f4129aa9a37cacc92"
   end
   def install
     downloaded = Dir["anvil-*"].first || "anvil"
